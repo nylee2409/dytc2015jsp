fb3079113d5eb0e5a7e1ddcd9d75e4795193a0cb
best2409 <best2409@naver.com>
2015-03-13 13:37:05 +0900

    First JSP/Servlet

---
+++ 01.TestProject/src/exam/chapter01/HelloServlet.java
@@ -0,0 +1,54 @@
+package exam.chapter01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+//@WebServlet("*.first")  // Annotation http://localhost:8080/hello.first
+//@WebServlet("/HelloServlet")  // Annotation http://localhost:8080/HelloServlet
+@WebServlet("/hello")  // Annotation http://localhost:8080/HelloServlet
+
+public class HelloServlet extends HttpServlet {  // 상속 
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * GET 방식으로 요청이 들어온 경우 자동 호출!
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		PrintWriter out = response.getWriter();
+		
+		out.print("Hello Servlet - GET");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * POST 방식으로 요청이 들어온 경우 자동 호출!
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		PrintWriter out = response.getWriter();
+		
+		out.print("Hello Servlet - POST");
+	}
+
+}
